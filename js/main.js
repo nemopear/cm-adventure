5a603702c94b890ae867ff8da1e602da3febdebb
nemopear <nemopear@gmail.com>
2017-06-21 18:01:02 +0700

    Map marker / map custom

--- js/main.js
+++ js/main.js
@@ -5,3 +5,34 @@ var myShuffle = new Shuffle(document.querySelector('.shuffle-container'), {
   sizer: '.js-shuffle-sizer', 
   gutterWidth: 15,
 });
+
+
+const ul = document.getElementById('authors'); // Get the list where we will place our authors
+const url = 'https://randomuser.me/api/?results=3'; // Get 10 random users
+
+  function createNode(element) {
+    return document.createElement(element); // Create the type of element you pass in the parameters
+  }
+
+  function append(parent, el) {
+    return parent.appendChild(el); // Append the second parameter(element) to the first one
+  }
+
+  fetch(url)
+  .then((resp) => resp.json())
+  .then(function(data) {
+    let authors = data.results;
+    return authors.map(function(author) {
+      let li = createNode('li'),
+          img = createNode('img'),
+          span = createNode('span');
+      img.src = author.picture.medium;
+      span.innerHTML = `${author.name.first} ${author.name.last}`;
+      append(li, img);
+      append(li, span);
+      append(ul, li);
+    })
+  })
+  .catch(function(error) {
+    console.log(error);
+  }); 
